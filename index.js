88cdeb4c4794fb48caece8a44d63ff653e0aa45d
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-20 23:28:04 +0000

    works pretty good rn, using twitter-lite

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const Twitter = require('twitter-lite');
 const client = new Twitter({
@@ -9,8 +11,11 @@ const client = new Twitter({
 });
 
 app.listen(5500, () => console.log('listening on port 5500'));
+app.use(cookieParser('bababooey'));
 
-app.use('/', express.static('test-site'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.get('/twitter', (req, res) => {
   client
@@ -38,7 +43,52 @@ app.get('/twitter/login', (req, res) => {
         userId: twRes.user_id,
         screenName: twRes.screen_name,
       });
+
+      userId = twRes.user_id;
+
+      res.cookie(
+        'twitterlog',
+        {
+          access_token_key: twRes.oauth_token,
+          access_token_secret: twRes.oauth_token_secret,
+        },
+        {
+          expires: 0,
+          signed: true,
+        }
+      );
+
       res.redirect('/');
     })
     .catch(console.error);
+});
+
+app.get('/twitter/pfp', (req, res) => {
+  if (!req.signedCookies.twitterlog || !req.signedCookies.twitterlog) {
+    res.send({
+      message: 'not logged in',
+    });
+    return;
+  }
+  const client = new Twitter({
+    version: '2',
+    extension: false,
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_KEY_SECRET,
+    access_token_key: req.signedCookies.twitterlog.access_token_key,
+    access_token_secret: req.signedCookies.twitterlog.access_token_secret,
+  });
+  client
+    .get('users/me', {
+      'user.fields': 'profile_image_url',
+    })
+    .then(twRes => {
+      res.send(twRes.data);
+    })
+    .catch(console.error);
+});
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('twitterlog');
+  res.redirect('/');
 });
